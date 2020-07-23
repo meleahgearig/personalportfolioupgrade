ffb23984153cd02e5b66304d0707cbce86f3c7f7
Meleah <meleahsanders@gmail.com>
2020-07-23 16:22:47 -0600

    fixed javascript and added some new styling

---
+++ main.js
@@ -0,0 +1,28 @@
+import { films } from './films.js';
+
+const filmsul = document.getElementById('filmsul');
+
+console.log(films)
+
+films.map(film => {
+    console.log(film)
+    
+    let li = createNode('li'),
+    img= createNode('img'),
+    span= createNode('span');
+
+
+    span.innerHTML = `${film.title} ${film.director}`
+
+    append(li, span);
+    append(filmsul, li);
+
+})
+
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el){
+    return parent.appendChild(el);
+}
